8fedde67d6eb7eee9e0a9fcea65c8458d6a71de4
massimo.daros <massimo.daros@external.cerved.com>
2021-07-22 18:44:06 +0200

    Adding Dependency Injection

--- devices-metrics-simulator/src/main/scala/mdaros/training/lagom/devices/simulator/DeviceMetricsSimulator.scala
+++ devices-metrics-simulator/src/main/scala/mdaros/training/lagom/devices/simulator/DeviceMetricsSimulator.scala
@@ -1,21 +1,21 @@
 package mdaros.training.lagom.devices.simulator
 
-import akka.Done
 import akka.actor.ActorSystem
-import akka.stream.alpakka.mqtt.scaladsl.MqttSink
-import akka.stream.alpakka.mqtt.{ MqttConnectionSettings, MqttMessage, MqttQoS }
-import akka.stream.scaladsl.{ Sink, Source }
+import akka.event.Logging
+import akka.stream.alpakka.mqtt.MqttMessage
+import akka.stream.scaladsl.Source
 import akka.util.ByteString
 import com.fasterxml.jackson.databind.json.JsonMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.softwaremill.macwire.wire
 import com.typesafe.config.ConfigFactory
-import mdaros.training.lagom.devices.simulator.config.ConfigurationKeys.{ CLIENT_ID_PREFIX, DEVICES_COUNT, MQTT_BROKER_HOST, MQTT_BROKER_PORT, TOPIC }
+import mdaros.training.lagom.devices.simulator.config.ConfigurationKeys.{ CLIENT_ID_PREFIX, DEVICES_COUNT, TOPIC }
+import mdaros.training.lagom.devices.simulator.sink.MqttSinkBuilder
 import mdaros.training.lagom.model.Measure
-import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
 
 import java.util.Date
 import scala.concurrent.duration.DurationInt
-import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.ExecutionContext
 import scala.language.postfixOps
 
 object DeviceMetricsSimulator extends App {
@@ -23,13 +23,14 @@ object DeviceMetricsSimulator extends App {
   implicit val actorSystem = ActorSystem ( "devices-metrics-simulator" )
   implicit val executionContext = ExecutionContext.Implicits.global
 
-  val config = ConfigFactory.load ( "application.conf" ).getConfig ( "devices-metrics-simulator" )
+  val logger = Logging ( actorSystem, getClass )
+
+  val config = ConfigFactory.load ( "application.conf" )
+    .getConfig ( "devices-metrics-simulator" )
 
   val topic          = config.getString ( TOPIC.key )
-  val mqttHost       = config.getString ( MQTT_BROKER_HOST.key )
-  val mqttPort       = config.getInt ( MQTT_BROKER_PORT.key )
-  val clientIdPrefix = config.getString ( CLIENT_ID_PREFIX.key )
   val numeDevices    = config.getInt ( DEVICES_COUNT.key )
+  val clientIdPrefix = config.getString ( CLIENT_ID_PREFIX.key )
 
   val jsonMapper = JsonMapper.builder ()
     .addModule ( DefaultScalaModule )
@@ -44,17 +45,20 @@ object DeviceMetricsSimulator extends App {
 
     val clientId = s"${clientIdPrefix}${index}"
 
-    val connectionSettings = MqttConnectionSettings ( s"tcp://${ mqttHost }:${ mqttPort }", clientId, new MemoryPersistence )
-
     val measuresSource = Source.tick ( 0 seconds, 5 seconds, new Date () )
       .map ( timestamp => Seq ( Measure ( clientId, "cpu.usage", timestamp, randomGenerator.nextDouble () * 100 ),
                                 Measure ( clientId, "mem.usage", timestamp, randomGenerator.nextDouble () * 100 ),
                                 Measure ( clientId, "disk.usage", timestamp, randomGenerator.nextDouble () * 100 ) ) )
       .map ( measure => MqttMessage ( topic, ByteString ( jsonMapper.writer ().writeValueAsString ( measure ) ) ) )
 
-    val mqttSink: Sink [ MqttMessage, Future [ Done ] ] = MqttSink ( connectionSettings, MqttQoS.ExactlyOnce )
+    val mqttSinkBuilder = wire [ MqttSinkBuilder ]
+    val mqttSink = mqttSinkBuilder.buildMqttSink ( config, index )
 
     // Run the flow
     measuresSource.runWith ( mqttSink )
+
+    logger.info ( s"Added simulated device clientId: $clientId" )
   } )
+
+  logger.info ( "DeviceMetricsSimulator started" )
 }
---
+++ devices-metrics-simulator/src/main/scala/mdaros/training/lagom/devices/simulator/sink/MqttSinkBuilder.scala
@@ -0,0 +1,26 @@
+package mdaros.training.lagom.devices.simulator.sink
+
+import akka.Done
+import akka.stream.alpakka.mqtt.scaladsl.MqttSink
+import akka.stream.alpakka.mqtt.{ MqttConnectionSettings, MqttMessage, MqttQoS }
+import akka.stream.scaladsl.Sink
+import com.typesafe.config.Config
+import mdaros.training.lagom.devices.simulator.config.ConfigurationKeys.{ CLIENT_ID_PREFIX, MQTT_BROKER_HOST, MQTT_BROKER_PORT }
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
+
+import scala.concurrent.Future
+
+class MqttSinkBuilder {
+
+  def buildMqttSink ( config: Config, index: Int ): Sink [ MqttMessage, Future [ Done ] ] = {
+
+    val mqttHost       = config.getString ( MQTT_BROKER_HOST.key )
+    val mqttPort       = config.getInt ( MQTT_BROKER_PORT.key )
+    val clientIdPrefix = config.getString ( CLIENT_ID_PREFIX.key )
+
+    val clientId = s"${clientIdPrefix}${index}"
+    val mqttConnectionSettings = MqttConnectionSettings ( s"tcp://${ mqttHost }:${ mqttPort }", clientId, new MemoryPersistence )
+
+    MqttSink ( mqttConnectionSettings, MqttQoS.ExactlyOnce )
+  }
+}
